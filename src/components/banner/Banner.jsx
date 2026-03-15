60e5e3e8ac2c5d8ab36aa4a57b79e2148979f3b4
DIPU DAS <ddipu0118@gmail.com>
2026-03-16 00:45:05 +0600

    header&footer done

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <div>
+                <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h3>
+                <button className='btn btn-primary'>test</button>
+            </div>
+            <div>
+                <img src="" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
